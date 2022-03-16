c72225c1dc15066eb49fdaeef84ccf85d4dd46f4
MrBlasyMSK <85447029+MrBlasyMSK@users.noreply.github.com>
2022-03-16 21:46:44 +0100

    Update FactionMasterBank.php

--- src/FactionMasterBank.php
+++ src/FactionMasterBank.php
@@ -171,8 +171,8 @@ class FactionMasterBank extends PluginBase implements Extension {
 		$this->saveResource('en_EN.yml');
 		$this->saveResource('tr_TR.yml');
 		$this->saveResource('ja_JP.yml');
-		$this->saveResource('es_ES.yml');
 		$this->saveResource('ru_RU.yml');
+		$this->saveResource('es_SPA.yml');
 		$this->saveResource('config.yml');
 		$this->config = new Config($this->getDataFolder() . "config.yml");
 		ConfigUpdater::checkUpdate($this, $this->config, "file-version", 2);
@@ -180,14 +180,14 @@ class FactionMasterBank extends PluginBase implements Extension {
 		ConfigUpdater::checkUpdate($this, new Config($this->getDataFolder() . "en_EN.yml", Config::YAML), "file-version", 3);
 		ConfigUpdater::checkUpdate($this, new Config($this->getDataFolder() . "tr_TR.yml", Config::YAML), "file-version", 2);
 		ConfigUpdater::checkUpdate($this, new Config($this->getDataFolder() . "ja_JP.yml", Config::YAML), "file-version", 1);
-		ConfigUpdater::checkUpdate($this, new Config($this->getDataFolder() . "es_ES.yml", Config::YAML), "file-version", 2);
 		ConfigUpdater::checkUpdate($this, new Config($this->getDataFolder() . "ru_RU.yml", Config::YAML), "file-version", 1);
+		ConfigUpdater::checkUpdate($this, new Config($this->getDataFolder() . "es_SPA.yml", Config::YAML), "file-version", 2);
 		$this->langConfig = [
 			"FR" => new Config($this->getDataFolder() . "fr_FR.yml", Config::YAML),
 			"EN" => new Config($this->getDataFolder() . "en_EN.yml", Config::YAML),
 			"TR" => new Config($this->getDataFolder() . "tr_TR.yml", Config::YAML),
 			"JP" => new Config($this->getDataFolder() . "ja_JP.yml", Config::YAML),
-			"SPA" => new Config($this->getDataFolder() . "es_ES.yml", Config::YAML),
+			"SPA" => new Config($this->getDataFolder() . "es_SPA.yml", Config::YAML),
 			"RU" => new Config($this->getDataFolder() . "ru_RU.yml", Config::YAML)
 		];
 	}
